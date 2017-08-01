bcac1a49efa913ee9ce79e7652e7e81c39fcf48f
premsaha24 <prem.saha@untroddenlabs.com>
2017-08-01 17:33:46 +0530

    completed uuid

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import abort
 from flask import make_response
 from flask import request
 from flask import url_for
-from flask.ext.httpauth import HTTPBasicAuth
+# from flask.ext.httpauth import HTTPBasicAuth
 import couchbase.subdocument as SD
 from couchbase.bucket import Bucket
 
-auth = HTTPBasicAuth()
+# auth = HTTPBasicAuth()
 app = Flask(__name__)
-cb = Bucket('couchbase:///task', username = 'prem.saha',password='untrodden123')
+# cb = Bucket('couchbase:///task', username = 'prem.saha',password='untrodden123')
 
 
 
@@ -45,8 +45,6 @@ cb = Bucket('couchbase:///task', username = 'prem.saha',password='untrodden123')
 #     }
 # ]
 
-tas = (cb.get('55',('55')).value
-tasks = tas['55']
 
 
 @app.route('/')
@@ -54,76 +52,76 @@ def index():
     return "Hello, World!"
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# @auth.login_required
-def get_tasks():
-    return jsonify(tasks)
-    # return jsonify({tasks : [make_public_task(task) for task in tasks]})
-
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-# @auth.login_required
-def get_task(task_id):
-    task = [task for task in tasks['55'] if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# @auth.login_required
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks['55'][-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks['55'].append(task)
-    cb.upsert('55',{tas})
-    return jsonify({'task': task}), 201
-
-# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 # # @auth.login_required
-# def update_task(task_id):
+# def get_tasks():
+#     return jsonify(tasks)
+#     # return jsonify({tasks : [make_public_task(task) for task in tasks]})
+
+
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+# # @auth.login_required
+# def get_task(task_id):
+#     task = [task for task in tasks['55'] if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
+
+# # @app.errorhandler(404)
+# # def not_found(error):
+# #     return make_response(jsonify({'error': 'Not found'}), 404)
+
+# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+# # @auth.login_required
+# def create_task():
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks['55'][-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks['55'].append(task)
+#     cb.upsert('55',{tas})
+#     return jsonify({'task': task}), 201
+
+# # @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+# # # @auth.login_required
+# # def update_task(task_id):
+# #     task = [task for task in tasks if task['id'] == task_id]
+# #     if len(task) == 0:
+# #         abort(404)
+# #     if not request.json:
+# #         abort(400)
+# #     if 'title' in request.json and type(request.json['title']) != unicode:
+# #         abort(400)
+# #     if 'description' in request.json and type(request.json['description']) is not unicode:
+# #         abort(400)
+# #     if 'done' in request.json and type(request.json['done']) is not bool:
+# #         abort(400)
+# #     task[0]['title'] = request.json.get('title', task[0]['title'])
+# #     task[0]['description'] = request.json.get('description', task[0]['description'])
+# #     task[0]['done'] = request.json.get('done', task[0]['done'])
+# #     return jsonify({'task': task[0]})
+
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+# # @auth.login_required
+# def delete_task(task_id):
 #     task = [task for task in tasks if task['id'] == task_id]
 #     if len(task) == 0:
 #         abort(404)
-#     if not request.json:
-#         abort(400)
-#     if 'title' in request.json and type(request.json['title']) != unicode:
-#         abort(400)
-#     if 'description' in request.json and type(request.json['description']) is not unicode:
-#         abort(400)
-#     if 'done' in request.json and type(request.json['done']) is not bool:
-#         abort(400)
-#     task[0]['title'] = request.json.get('title', task[0]['title'])
-#     task[0]['description'] = request.json.get('description', task[0]['description'])
-#     task[0]['done'] = request.json.get('done', task[0]['done'])
-#     return jsonify({'task': task[0]})
+#     tasks.remove(task[0])
+#     return jsonify({'result': True})
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-# @auth.login_required
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
-
-# def make_public_task(task):
-#     new_task = {}
-#     for field in task:
-#         if field == 'id':
-#             new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-#         else:
-#             new_task[field] = task[field]
-#     return new_task
+# # def make_public_task(task):
+# #     new_task = {}
+# #     for field in task:
+# #         if field == 'id':
+# #             new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+# #         else:
+# #             new_task[field] = task[field]
+# #     return new_task
 
  
 
--- restfulapi.py
+++ restfulapi.py
@@ -13,66 +13,72 @@ from flask.ext.httpauth import HTTPBasicAuth
 import couchbase.subdocument as SD
 from couchbase.bucket import Bucket
 import pdb
-flag = 0
 app = Flask(__name__, static_url_path="")
 api = Api(app)
 
 cb = Bucket('couchbase:///task', username = 'prem.saha',password='untrodden123')
 
 task_fields = {
-    'title': fields.String,
-    'id': fields.Integer,
-    'description': fields.String,
-    'done': fields.Boolean
+    'text': fields.String,
+    'user_id': fields.Integer,
+    'created_at': fields.String,
+    'time': fields.String
 }
 
 
 class TaskListAPI(Resource):
+   
+    a = None
+
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type=str, required=True,
+        self.reqparse.add_argument('text', type=str, required=True,
                                    help='No task title provided',
                                    location='json')
-        self.reqparse.add_argument('id', type=int, default="",
+        self.reqparse.add_argument('user_id', type=int, default="",
                                    location='json')
-        self.reqparse.add_argument('description', type=str, default="",
+        self.reqparse.add_argument('created_at', type=str, default="",
                                    location='json')
-        self.reqparse.add_argument('done', type=bool, default="",
+        self.reqparse.add_argument('time', type=str, default="",
                                    location='json')
-
         super(TaskListAPI, self).__init__()
 
     def post(self):
         
         store_id = uuid.uuid4().hex
+        
+        
         a = store_id
+        
+       
         args = self.reqparse.parse_args()
          
         task = {
-
-            'id': args['id'],
-            'title': args['title'],
-            'description': args['description'],
-            'done': args['done']
-
+            'text': args['text'],
+            'user_id': args['user_id'],
+            'created_at': args['created_at'],
+            'time': args['time'],
         }
-
         TaskListAPI.response = a 
         cb.upsert(store_id,{'id':store_id,'comn':[task]})
         rv = cb.get(store_id)
         return jsonify(rv.value)
-              
+       
+  
+       
     def put(self):  
         
         sub_id = TaskListAPI.response
+      
         args = self.reqparse.parse_args()
 
         task = {
-            'id': args['id'],
-            'title': args['title'],
-            'description': args['description'],
-            'done': args['done']
+            'text': args['text'],
+            'user_id': args['user_id'],
+            'created_at': args['created_at'],
+            'time': args['time'],
+
         }
 
         cb.mutate_in(sub_id, SD.array_append('comn',task))
